6d2a45ccc77f073f62bc469f1f88225605858252
Robin Saini <Robin Saini@RobS>
2021-05-13 21:29:12 +0530

    __Updated

--- spring.boot.microservice/src/main/java/com/robin/java/controller/MainController.java
+++ spring.boot.microservice/src/main/java/com/robin/java/controller/MainController.java
@@ -1,5 +1,9 @@
 package com.robin.java.controller;
 
+import java.util.StringJoiner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +16,13 @@ public class MainController {
 		return "Welcome to the Springboot Microservice!";
 	}
 	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@GetMapping("/services")
+	public String check() {
+		StringJoiner js = new StringJoiner(",","[","]");
+		discoveryClient.getServices().stream().forEach(serv -> js.add(serv));
+		return js.toString();
+	}
 }
